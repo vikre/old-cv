223d05d77e1b975763db5a15d97e75695a95ad2b
Linn Vikre <vikre.90@gmail.com>
2016-03-11 18:01:41 +0100

    jadda

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,6 @@ var deploy      = require('gulp-gh-pages');
  * Push build to gh-pages
  */
 gulp.task('deploy', function () {
-  return gulp.src("./src/client/**/*")
+  return gulp.src("./src/client/public/**/*")
     .pipe(deploy())
 });
